1994efe42efeafd9d7a991dd00bfe59546bf3f3b
lanae <rlfoard@gmail.com>
2018-04-25 10:51:34 -0700

    added gifs

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,55 +1,55 @@
 var friends = [{
         "name": "Pam",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo": "https://media.giphy.com/media/kORa7iZkiHepO/giphy.gif",
         "scores": [
             3, 2, 3, 4, 4, 1, 3, 3, 3, 2
         ]
     },
     {
         "name": "Jim",
-        "photo": "https://www.google.com/imgres?imgurl=https%3A%2F%2Faz616578.vo.msecnd.net%2Ffiles%2F2016%2F03%2F04%2F635927223188301534-622930799_JIM.png&imgrefurl=http%3A%2F%2Fwww.dailyedge.ie%2Fjim-the-us-office-3576726-Sep2017%2F&docid=9jeDdknNg8C-TM&tbnid=aAH06NI_YkktnM%3A&vet=10ahUKEwjSscvmodTaAhVV12MKHeWeBFsQMwjIASgCMAI..i&w=1024&h=576&bih=590&biw=1280&q=the%20office%20jim&ved=0ahUKEwjSscvmodTaAhVV12MKHeWeBFsQMwjIASgCMAI&iact=mrc&uact=8",
+        "photo": "https://media.giphy.com/media/agd5h8tLLOx32/giphy.gif",
         "scores": [
             4, 4, 4, 1, 4, 1, 3, 3, 3, 4
         ]
     },
     {
         "name": "Michael",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo": "https://media.giphy.com/media/vzoYZ7Ef9clbO/giphy.gif",
         "scores": [
             3, 2, 3, 1, 3, 5, 1, 1, 5, 5
         ]
     },
     {
         "name": "Dwight",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo": "https://media.giphy.com/media/134DVXcD94sOWI/giphy.gif",
         "scores": [
             4, 4, 2, 2, 2, 5, 5, 3, 1, 5
         ]
     },
     {
         "name": "Kelly",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo": "https://media.giphy.com/media/LgUCxBvIyqqmQ/giphy.gif",
         "scores": [
             3, 2, 1, 2, 1, 4, 4, 1, 4, 5
         ]
     },
     {
         "name": "Ryan",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo": "https://media.giphy.com/media/DUz7RkQlIuAOA/giphy.gif",
         "scores": [
-            5, 2, 3, 3, 3, 4, 4, 1, 3, 5
+            4, 2, 3, 3, 3, 4, 4, 1, 3, 5
         ]
     },
     {
         "name": "Toby",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo": "https://media.giphy.com/media/VLBBz4jxdyeic/giphy.gif",
         "scores": [
             1, 5, 5, 4, 5, 1, 2, 3, 5, 3
         ]
     },
     {
-        "name": "Phyllis",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Kevin",
+        "photo": "https://media.giphy.com/media/3K7MTeqALjvSo/giphy.gif",
         "scores": [
             2, 3, 5, 5, 5, 2, 3, 3, 3, 4
         ]
