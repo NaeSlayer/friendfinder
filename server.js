479e16316a1bfcb78d676dfe3b8b17572d841890
lanae <rlfoard@gmail.com>
2018-04-25 11:13:11 -0700

    added photo to home.html

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var path = require("path");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
+// Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({
